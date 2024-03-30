640893714dd1f7ae170d929f49da99d429639fba
Habiba Parveen <habibasyd532@gamil.com>
2024-03-30 00:36:15 -0700

    done

--- app/src/main/java/com/example/attendancemanager/LoginActivity.java
+++ app/src/main/java/com/example/attendancemanager/LoginActivity.java
@@ -19,6 +19,6 @@ public class LoginActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
-        });habiba
+        });russel
     }
 }
