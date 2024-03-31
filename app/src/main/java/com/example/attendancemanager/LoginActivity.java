02f91fc17d509795ab91f5b95321ee54f32e40f2
Habiba Parveen <habibasyd532@gamil.com>
2024-03-30 23:38:16 -0700

    done

--- app/src/main/java/com/example/attendancemanager/LoginActivity.java
+++ app/src/main/java/com/example/attendancemanager/LoginActivity.java
@@ -1,24 +1,88 @@
 package com.example.attendancemanager;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
 public class LoginActivity extends AppCompatActivity {
 
+    private FirebaseAuth auth;
+    private EditText loginEmail, loginPassword;
+    private TextView signupRedirectText;
+    private Button loginButton;
+
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_login);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        }); "Nalayuck"
+
+        auth = FirebaseAuth.getInstance();
+        loginEmail = findViewById(R.id.login_email);
+        loginPassword = findViewById(R.id.login_password);
+        loginButton = findViewById(R.id.login_button);
+        signupRedirectText = findViewById(R.id.signupRedirectText);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = loginEmail.getText().toString();
+                String password = loginPassword.getText().toString();
+
+                if(!email.isEmpty() && Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+                    if (!password.isEmpty()) {
+                         auth.signInWithEmailAndPassword(email,password)
+                                 .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                                     @Override
+                                     public void onSuccess(AuthResult authResult) {
+                                         Toast.makeText(LoginActivity.this, "login Successfully", Toast.LENGTH_SHORT).show();
+                                         startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                                         finish();
+                                     }
+                                 }).addOnFailureListener(new OnFailureListener() {
+                                     @Override
+                                     public void onFailure(@NonNull Exception e) {
+                                         Toast.makeText(LoginActivity.this, "login failed", Toast.LENGTH_SHORT).show();
+                                     }
+                                 });
+
+                    } else {
+                        loginPassword.setError("Password cannot be empty");
+                    }
+                } else if (email.isEmpty()){
+                    loginEmail.setError("Email cannot be empty");
+                } else {
+                    loginEmail.setError("Please enter valid email");
+
+                }
+
+            }
+        });
+        signupRedirectText.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            startActivity(new Intent(LoginActivity.this,SignupActivity.class));
+        }
+    });
     }
 }
